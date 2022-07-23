51aa353fbce2e2f73742f067e413679376f81555
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-07-23 11:17:30 -0700

    Add container div and amend for loop to put all div rows inside container

--- EASscript.js
+++ EASscript.js
@@ -1,14 +1,18 @@
+const container = document.createElement('div');
+      document.body.appendChild(container);
+      container.classList.add('container');
+
 for (i = 0; i < 16; i++) {
     
     var divRow = document.createElement('div');
-    document.body.appendChild(divRow);
-    divRow.classList.add("divRow");
+    container.appendChild(divRow);
+    divRow.classList.add('divRow');
 
     for (j = 0; j < 16; j++) {
 
         var divColumn = document.createElement('div');
         divRow.appendChild(divColumn);
-        divColumn.classList.add("divColumn");
+        divColumn.classList.add('divColumn');
     }
 }
 
