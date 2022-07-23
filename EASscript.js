a475028ec0234095b494903dacdb86b64f9197f5
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-07-23 10:37:43 -0700

    Add class to divRow and divColumn

--- EASscript.js
+++ EASscript.js
@@ -2,11 +2,13 @@ for (i = 0; i < 16; i++) {
     
     var divRow = document.createElement('div');
     document.body.appendChild(divRow);
+    divRow.classList.add("divRow");
 
     for (j = 0; j < 16; j++) {
 
         var divColumn = document.createElement('div');
         divRow.appendChild(divColumn);
+        divColumn.classList.add("divColumn");
     }
 }
 
