4f78093d80b8da245678ce8c97004a736ab7e8c6
Yousuf Rabani <m.yousuf.rabani@gmail.com>
2022-07-26 15:16:02 -0700

    Clean up presentation, empty lines and add comments

--- EASscript.js
+++ EASscript.js
@@ -1,8 +1,43 @@
 const container = document.createElement('div');
 document.body.appendChild(container);
 container.classList.add('container');
-
 var numGrids = 0; 
+
+
+//Generate initial 16x16 grid when page is loaded
+
+for (i = 0; i < 16; i++) {
+
+    var divRow = document.createElement('div');
+    container.appendChild(divRow);
+    divRow.classList.add('divRow');
+
+    for (j = 0; j < 16; j++) {
+
+        var divColumn = document.createElement('div');
+        divRow.appendChild(divColumn);
+        divColumn.classList.add('divColumn');
+        divColumn.setAttribute('id', `${i}${i}${j}`);
+    }
+}
+
+const grids = document.querySelectorAll('.divColumn');
+// addEventListener is a method on the HTML Element so it needs to be accessed using dot notation. 
+// The callback receives "event" as a parameter which contains lots of info about the user interaction
+// event.target gets you to the HTML element that was interacted, it has an "id" property which can
+// be passed to changeClass()
+grids.forEach((grid) => grid.addEventListener('mouseover', (event) => {
+    changeClass(event.target.id);
+}));
+
+const clearButton = document.querySelector('#btn');
+clearButton.addEventListener('click', () => {
+    removeGrid();
+    numGrids = userInput();
+    genGrid(numGrids);
+    changeGridSize(numGrids);
+    paintGrids();
+});
 
 function userInput() {
     var numGrids = parseInt(prompt("Enter a number between 16 to 100: "), 10);
@@ -17,7 +52,6 @@ function removeGrid() {
     const colorGrids = document.querySelectorAll('.hoveredColumn')
     colorGrids.forEach((grid) => grid.remove());
 }
-
 
 function genGrid(numGrids) {
     if (numGrids < 16 || numGrids > 100 || !Number.isInteger(numGrids)) {
@@ -40,37 +74,11 @@ function genGrid(numGrids) {
     }
 }
 
-
-for (i = 0; i < 16; i++) {
-
-    var divRow = document.createElement('div');
-    container.appendChild(divRow);
-    divRow.classList.add('divRow');
-
-    for (j = 0; j < 16; j++) {
-
-        var divColumn = document.createElement('div');
-        divRow.appendChild(divColumn);
-        divColumn.classList.add('divColumn');
-        divColumn.setAttribute('id', `${i}${i}${j}`);
-    }
-}
-
 function changeClass(elemId) {
     const box = document.getElementById(elemId);
     box.classList.remove('divColumn');
     box.classList.add('hoveredColumn');
 }
-
-const grids = document.querySelectorAll('.divColumn');
-
-// addEventListener is a method on the HTML Element so it needs to be accessed using dot notation. 
-// The callback receives "event" as a parameter which contains lots of info about the user interaction
-// event.target gets you to the HTML element that was interacted, it has an "id" property which can
-// be passed to changeClass()
-grids.forEach((grid) => grid.addEventListener('mouseover', (event) => {
-    changeClass(event.target.id);
-}));
 
 function paintGrids() {
     const grids = document.querySelectorAll('.divColumn');
@@ -87,13 +95,6 @@ function changeGridSize (numGrids) {
     })
 }
 
-const clearButton = document.querySelector('#btn');
-clearButton.addEventListener('click', () => {
-    removeGrid();
-    numGrids = userInput();
-    genGrid(numGrids);
-    changeGridSize(numGrids);
-    paintGrids();
-});
+
 
 
